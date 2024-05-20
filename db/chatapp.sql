caab23e7e4d4737c1734d003b38cf8498800e1cf
Rober <elroberbtw@gmail.com>
2024-05-21 00:18:38 +0200

    base de datos actualizada

--- db/chatapp.sql
+++ db/chatapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2024 at 12:58 AM
+-- Generation Time: May 21, 2024 at 12:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `icono` varchar(255) NOT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id` int(11) NOT NULL,
+  `id_remitente` int(11) DEFAULT NULL,
+  `id_destinatario` int(11) DEFAULT NULL,
+  `id_grupo` int(11) DEFAULT NULL,
+  `contenido` text DEFAULT NULL,
+  `tipo_contenido` enum('texto','imagen','video','archivo') DEFAULT NULL,
+  `ruta_archivo` varchar(255) DEFAULT NULL,
+  `fecha_envio` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mensajes_privados`
+--
+
+CREATE TABLE `mensajes_privados` (
+  `id` int(11) NOT NULL,
+  `id_usuario_1` int(11) DEFAULT NULL,
+  `id_usuario_2` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mensajes_privados`
+--
+
+INSERT INTO `mensajes_privados` (`id`, `id_usuario_1`, `id_usuario_2`) VALUES
+(9, 19, 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `miembros_grupos`
+--
+
+CREATE TABLE `miembros_grupos` (
+  `id` int(11) NOT NULL,
+  `id_grupo` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reseteo_contraseñas`
+--
+
+CREATE TABLE `reseteo_contraseñas` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `fecha_expiracion` datetime NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -32,6 +107,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
+  `imagen_perfil` varchar(255) NOT NULL DEFAULT '''porDefecto.png''',
   `token` varchar(255) DEFAULT NULL,
   `activo` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -40,12 +116,51 @@ CREATE TABLE `usuarios` (
 -- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `email`, `contraseña`, `token`, `activo`) VALUES
-(16, 'Rober', 'asistentechatapp@gmail.com', '$2y$10$yUJpKlnqT5eQtwjZwI.FV.tGgvEh3ve./Fq/csd.gtC00D9pPoll2', NULL, 1);
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `contraseña`, `imagen_perfil`, `token`, `activo`) VALUES
+(18, 'Rober', 'asistentechatapp@gmail.com', '$2y$10$tjuWp3EbTE5TMnZ1vpEoC.qJM87c0fp4lfhbhlxp21YBKl0XOHvuq', '\'porDefecto.png\'', NULL, 1),
+(19, 'a', 'a@a.com', '$2y$10$d2PuqmXA8BvEKfeQgKfSbOBFPv.1QsuZUm0LedQbw91/KefstARjy', '\'porDefecto.png\'', NULL, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_remitente` (`id_remitente`),
+  ADD KEY `id_destinatario` (`id_destinatario`),
+  ADD KEY `id_grupo` (`id_grupo`);
+
+--
+-- Indexes for table `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_usuario` (`id_usuario_1`,`id_usuario_2`),
+  ADD KEY `chat` (`id_usuario_2`);
+
+--
+-- Indexes for table `miembros_grupos`
+--
+ALTER TABLE `miembros_grupos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_grupo` (`id_grupo`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indexes for table `reseteo_contraseñas`
+--
+ALTER TABLE `reseteo_contraseñas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `usuarios`
@@ -61,10 +176,72 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `miembros_grupos`
+--
+ALTER TABLE `miembros_grupos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `reseteo_contraseñas`
+--
+ALTER TABLE `reseteo_contraseñas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `grupos`
+--
+ALTER TABLE `grupos`
+  ADD CONSTRAINT `grupos_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`id_remitente`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_ibfk_2` FOREIGN KEY (`id_destinatario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_ibfk_3` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id`);
+
+--
+-- Constraints for table `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  ADD CONSTRAINT `mensajes_privados_ibfk_1` FOREIGN KEY (`id_usuario_1`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_privados_ibfk_2` FOREIGN KEY (`id_usuario_2`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `miembros_grupos`
+--
+ALTER TABLE `miembros_grupos`
+  ADD CONSTRAINT `miembros_grupos_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id`),
+  ADD CONSTRAINT `miembros_grupos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
